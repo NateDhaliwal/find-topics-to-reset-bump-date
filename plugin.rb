71e0cac9a41675618a0b33d151ad5b8f3b5188f3
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-05-29 11:06:40 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,42 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: find-topics-to-reset-bump-date
 # about: TODO
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: NateDhaliwal
 # url: TODO
-# required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+require "net/http"
+require "json"
+require "uri"
+
+enabled_site_setting :enable_find_topics_to_reset_bump_date
 
 module ::MyPluginModule
   PLUGIN_NAME = "discourse-plugin-name"
 end
 
-require_relative "lib/my_plugin_module/engine"
+#require_relative "lib/my_plugin_module/engine"
 
-after_initialize do
-  # Code which should run after Rails has finished booting
+after_initialize doafter_initialize do
+  class ::Jobs::FindTopics < Jobs::Scheduled
+    every 12.hours # Run everyday
+
+    def execute(args)
+      topics_list = "#{Discourse.base_url}/latest.json"
+      json = api_request(topics_list)
+      
+      json["topic_list"]["topics"].each do |topic_data|
+        
+      end
+    end
+    
+    def api_request(url)
+      uri = URI(url)
+      response = Net::HTTP.get_response(uri)
+
+      JSON.parse(response.body) if response.kind_of? Net::HTTPSuccess
+    end
+  end
 end
