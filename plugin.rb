025a28cec0863bc8f5ed591b936829928e03bd86
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-05-29 12:00:01 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -19,17 +19,51 @@ end
 
 #require_relative "lib/my_plugin_module/engine"
 
-after_initialize doafter_initialize do
+after_initialize do
   class ::Jobs::FindTopics < Jobs::Scheduled
     every 12.hours # Run everyday
 
     def execute(args)
       topics_list = "#{Discourse.base_url}/latest.json"
       json = api_request(topics_list)
+      topics_need_to_reset = []
       
       json["topic_list"]["topics"].each do |topic_data|
-        
+        if topic_data["last_posted_at"] != topic_data["bumped_at"]
+          topics_need_to_reset.push({"topic_id" => topic_data["id"], "topic_title" => topic_data["title"], "topic_url" => "#{Discourse.base_url}/t/-/#{topic_data["id"}"})
+        end
       end
+        
+      # Find the admin group
+      admin_group = Group.find_by(name: "admins")
+      
+      # Compose your PM details
+      title = SiteSetting.reset_bump_date_pm_title
+      raw = SiteSetting.reset_bump_date_pm_body
+      rawTopicsToResetBumpDate = ""
+        
+      if SiteSetting.send_pm_when_no_topics_to_reset_bump_date == true 
+        if topics_need_to_reset.length() == 0
+          raw = SiteSetting.no_topics_to_reset_bump_date_pm_body
+        else
+          raw = SiteSetting.reset_bump_date_pm_body
+        end
+      end
+
+      topics_need_to_reset.each do |topic_data|
+        
+      
+      # The system user often sends automated messages
+      system_user = Discourse.system_user
+      
+      # Send the PM to the admin group
+      PostCreator.create!(
+        system_user,
+        title: title,
+        raw: raw,
+        archetype: Archetype.private_message,
+        target_group_names: [admin_group.name]
+      )
     end
     
     def api_request(url)
