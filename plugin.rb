11477f3413dd2028e1d6db46207020ace146b718
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-05-29 22:02:43 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -15,6 +15,6 @@ enabled_site_setting :enable_find_topics_to_reset_bump_date
 
 after_initialize do
   %w[
-    ../app/jobs/scheduled/find_topics_to_reset_bump.rb
+    ../app/jobs/scheduled/find_topics_to_reset.rb
   ].each { |path| require File.expand_path(path, __FILE__) }
 end
